61dfadffea0d4036e03fbe97952e0c292b0a5f7f
carmi <carmi@CERMINA_MAGED>
2025-03-08 14:53:13 +0200

    Base Entities

---
+++ LinkDev.IKEA.DAL/Common/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Common.Entities
+{
+    class BaseEntity<TKey> where TKey:IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
