fcd3fa3f742c76ee58ee4e2e9a4f103e9c729384
carmi <carmi@CERMINA_MAGED>
2025-03-08 16:21:20 +0200

    ApplicationDbContext

---
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LinkDev.IKEA.DAL.Common.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data
+{
+    class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server =.;Database =Company;Trusted_Connection =True ; Encrypt =True;TrustServerCertificate =true");
+        }
+        public DbSet<Department>Departments { get; set; }
+    }
+}
