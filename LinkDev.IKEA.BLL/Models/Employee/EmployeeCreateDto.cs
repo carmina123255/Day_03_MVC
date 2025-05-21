302b314f73984a854edce5df594c323e056c39d6
carmi <carmi@CERMINA_MAGED>
2025-05-21 21:18:52 +0300

    Employee Module -DTO

---
+++ LinkDev.IKEA.BLL/Models/Employee/EmployeeCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using LinkDev.IKEA.DAL.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.BLL.Models.Employee
+{
+    public record EmployeeCreateDto(int Id, string FirstName,
+     string LastName, string? Address,
+      decimal Salary, string? Email,string? PhoneNumber, DateOnly BirthOfDate
+  , Gender Gender, EmployeeType EmployeeType,
+     int? DepartmentId);
+
+  
+}
+
